0d4dc5fc70952a10b168eb09d6f7ac4d8a009606
Ross MacArthur <ross@macarthur.io>
2021-08-30 18:03:28 +0200

    Add GitHub Action

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import * as core from "@actions/core";
+import * as setup from "@extractions/setup-crate";
+
+async function main() {
+  try {
+    const owner = core.getInput("owner", { required: true });
+    const name = core.getInput("name", { required: true });
+    const versionSpec = core.getInput("version");
+    const tool = await setup.checkOrInstallTool({
+      owner,
+      name,
+      versionSpec,
+    });
+    core.addPath(tool.dir);
+    core.info(`Successfully setup ${tool.name} v${tool.version}`);
+  } catch (err) {
+    core.setFailed(err.message);
+  }
+}
+
+main();
